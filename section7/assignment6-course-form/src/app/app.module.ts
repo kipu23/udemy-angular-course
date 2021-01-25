7394564e3521376286c97fe0fdb871cf813fc4e9
uic87769 <krisztian.szep-EXT@continental-corporation.com>
2021-01-25 15:28:39 +0100

    assignment6 implementation

--- section7/assignment6-course-form/src/app/app.module.ts
+++ section7/assignment6-course-form/src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CourseFormComponent } from './course-form/course-form.component';
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CourseFormComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
